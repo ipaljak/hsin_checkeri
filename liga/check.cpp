641bede20f0aea492fae2cdf0cd557aa59aa4db9
ipaljak <ipaljak@gmail.com>
2015-10-18 14:32:42 +0200

    Dodan presentation error, testirano.

--- liga/check.cpp
+++ liga/check.cpp
@@ -22,18 +22,28 @@ FILE *in, *out, *user;
 
 char ok[] = "Correct!";
 char wa[] = "Wrong answer, some lines are not correct!";
+char pres_error[] = "Output data is not correctly formatted!";
 
 int n;
 int a[MAXN], b[MAXN];
+
+inline void message(double pts, char *txt) {
+    printf("%lf\n%s\n", pts, txt);
+}
+
+inline bool check_end() {
+    char dummy[2];
+    if (fscanf(user, "%1s", dummy) == 1) {
+        message(0, pres_error);
+        return true;
+    }
+    return false;
+}
 
 inline bool check_line() {
     for (int i = 0; i < 5; ++i)
         if (a[i] != b[i]) return false;
     return true;
-}
-
-inline void message(double pts, char *txt) {
-    printf("%lf\n%s\n", pts, txt);
 }
 
 int main(int argc, char **argv) {
@@ -43,18 +53,28 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    in = fopen(argv[1], "r"); assert(in != NULL);
-    out = fopen(argv[2], "r"); assert(out != NULL);
-    user = fopen(argv[3], "r"); assert(user != NULL);
+    in = fopen(argv[1], "r");
+    out = fopen(argv[2], "r");
+    user = fopen(argv[3], "r");
+
+    assert(in && out && user);
 
     fscanf(in, "%d", &n);
     
     int pass = 0;
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < 5; ++j) fscanf(out, "%d", &a[j]);
-        for (int j = 0; j < 5; ++j) fscanf(user, "%d", &b[j]);
+        for (int j = 0; j < 5; ++j) {
+            if (fscanf(user, "%d", &b[j]) != 1) {
+                message(0, pres_error);
+                return 0;
+            }
+        }
         pass += check_line();
     }
+
+    if (check_end()) 
+        return 0;
 
     if (pass == n)
         message(1, ok);
